cce9c67ede81bf278b01faba03f2fee658000551
lichaojiang <lichaojiang@360.cn>
2019-03-25 14:06:55 +0800

    调整

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Vue-jigsawPuzzle
+
 Jigsaw puzzle based on Vue  
 基于Vue的拼图小游戏  
   
 带有打乱时很炫酷的动画  
 ![example](https://github.com/usecodelee/Vue-jigsawPuzzle/blob/master/img/20190325.gif)
+
+## 使用：
+
+1. npm install
+
+2. npm run serve
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="box">
-    <button @click="shuffle">重置</button>
-    <transition-group name="cell" tag="div" class="container">
-      <div
-        @click.prevent="clickBlock(index)"
-        v-for="(puzzle,index) in puzzles"
-        :key="puzzle"
-        v-text="puzzle"
-        :class="puzzle === ''? 'cell cells':'cell'"
-      ></div>
-    </transition-group>
-  </div>
+    <div class="box">
+        <button @click="shuffle">重置</button>
+        <transition-group
+            name="cell"
+            tag="div"
+            class="container"
+        >
+            <div
+                @click.prevent="clickBlock(index)"
+                v-for="(puzzle,index) in puzzles"
+                :key="puzzle"
+                v-text="puzzle"
+                :class="puzzle === ''? 'cell cells':'cell'"
+            ></div>
+        </transition-group>
+    </div>
 </template>
 
 <script>
@@ -28,42 +32,40 @@ export default {
       this.puzzles = Array.apply(null, { length: 8 }).map(function(_, index) {
         return (index % 8) + 1;
       });
-      this.puzzles.push("");
+      this.puzzles.push('');
       this.shuffle();
     },
     // 点击方块
     clickBlock(index) {
-      let curIndex = this.puzzles[index],
-        leftIndex = this.puzzles[index - 1],
-        rightIndex = this.puzzles[index + 1],
-        topIndex = this.puzzles[index - 3],
-        bottomIndex = this.puzzles[index + 3];
+      let curIndex = this.puzzles[index];
+      let leftIndex = this.puzzles[index - 1];
+      let rightIndex = this.puzzles[index + 1];
+      let topIndex = this.puzzles[index - 3];
+      let bottomIndex = this.puzzles[index + 3];
 
-      if (leftIndex === "" && index % 3) {
+      if (leftIndex === '' && index % 3) {
         this.$set(this.puzzles, index - 1, curIndex);
-        this.$set(this.puzzles, index, "");
-      } else if (rightIndex === "" && 2 !== index % 3) {
+        this.$set(this.puzzles, index, '');
+      } else if (rightIndex === '' && 2 !== index % 3) {
         this.$set(this.puzzles, index + 1, curIndex);
-        this.$set(this.puzzles, index, "");
-      } else if (topIndex === "") {
+        this.$set(this.puzzles, index, '');
+      } else if (topIndex === '') {
         this.$set(this.puzzles, index - 3, curIndex);
-        this.$set(this.puzzles, index, "");
-      } else if (bottomIndex === "") {
+        this.$set(this.puzzles, index, '');
+      } else if (bottomIndex === '') {
         this.$set(this.puzzles, index + 3, curIndex);
-        this.$set(this.puzzles, index, "");
+        this.$set(this.puzzles, index, '');
       }
 
       this.pass();
     },
 
     pass() {
-      if (this.puzzles[8] === "") {
+      if (this.puzzles[8] === '') {
         const newPuzzles = this.puzzles.slice(0, 8);
-
         const isPass = newPuzzles.every((e, i) => e === i + 1);
-
         if (isPass) {
-          alert("666！");
+          alert('666！');
         }
       }
     }
@@ -76,38 +78,39 @@ export default {
 
 <style>
 .box {
-  width: 400px;
-  margin: 60px auto 0;
+    width: 400px;
+    margin: 60px auto 0;
 }
 
 .container {
-  display: flex;
-  flex-wrap: wrap;
-  width: 306px;
-  margin-top: 10px;
-  border: 1px solid #ccc;
+    display: flex;
+    flex-wrap: wrap;
+    width: 306px;
+    margin-top: 10px;
+    border: 1px solid #ccc;
 }
 .cell {
-  color: #fff;
-  font-size: 20px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 100px;
-  height: 100px;
-  margin: 1px;
-  -moz-box-shadow: 0px 0px 3px #333333;
-  -webkit-box-shadow: 0px 0px 3px #333333;
-  box-shadow: 0px 0px 3px #333333;
-  background-color: #996666;
+    color: #fff;
+    font-size: 20px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100px;
+    height: 100px;
+    margin: 1px;
+    -moz-box-shadow: 0px 0px 3px #333333;
+    -webkit-box-shadow: 0px 0px 3px #333333;
+    box-shadow: 0px 0px 3px #333333;
+    background-color: #996666;
 }
 .cells {
-  background-color: #fff;
-  -moz-box-shadow: 0px 0px 0px #333333;
-  -webkit-box-shadow: 0px 0px 0px #333333;
-  box-shadow: 0px 0px 0px #333333;
+    background-color: #fff;
+    -moz-box-shadow: 0px 0px 0px #333333;
+    -webkit-box-shadow: 0px 0px 0px #333333;
+    box-shadow: 0px 0px 0px #333333;
+    z-index: -999;
 }
 .cell-move {
-  transition: transform 0.5s;
+    transition: transform 0.5s;
 }
 </style>
